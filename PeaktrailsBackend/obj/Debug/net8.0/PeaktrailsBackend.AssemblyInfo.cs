da6f477400f7aba87c10e90d0d78c65775aabf8e
Yens Bastiaens$ <yens.bastiaens@student.pxl.be>
2024-12-03 21:11:22 +0100

    fix wrong files

--- PeaktrailsBackend/obj/Debug/net8.0/PeaktrailsBackend.AssemblyInfo.cs
+++ PeaktrailsBackend/obj/Debug/net8.0/PeaktrailsBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeaktrailsBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7aef8b0937aec8e3ceeb1bf58f38b2db3f4193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a972a38cd968c512fe641c9cb1c62aba88964a10")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeaktrailsBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeaktrailsBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
